a9aff0702c0f50d1257d0cd2307a490dc7c55c01
ABHIN <abhindev.es@gmail.com>
2023-04-28 11:58:37 +0530

    Docker

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
+from functools import lru_cache
 import pathlib
 
+class Settings(BaseSettings):
+    debug: bool = False
+
+    class config:
+        env_file = ".env"
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+DEBUG = get_settings().debug
 app = FastAPI()
 BASE_DIR = pathlib.Path(__file__).parent
 templates = Jinja2Templates(directory=str(BASE_DIR/"templates"))
 
 @app.get("/",response_class=HTMLResponse) # http GET
-def home_view(request:Request):
+def home_view(request:Request, settings:Settings = Depends(get_settings)):
+    print(settings.debug)
     return templates.TemplateResponse("home.html",{"request":request, "abc":123} )
 
 @app.post("/") # http POST
